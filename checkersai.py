c083ab01769acf1a9fed533b15afeed53562186d
Khoa Hoang <whoskhoahoang@gmail.com>
2020-03-02 21:03:00 +0900

    Correct player_color logic for minimax().
    TODO: Verify minimax correctness. Implement
          alpha-beta pruning version.

--- checkersai.py
+++ checkersai.py
@@ -46,44 +46,22 @@ def minimax(gamestate, cpu_color, depth):
     type cpu_color: str
     type depth: int
     """
-    print("\n\n")
-    print("IN minimax")
-    print(depth)
-    print(gamestate)
-    print(cpu_color)
-    print("\n\n")
-
-    best_move = None
-    player_color = "B" if cpu_color == "R" else "B"
+    best_move = None    
+    player_color = "B" if cpu_color == "R" else "R"
 
     #Base case of Mini Max
     if depth == 0 or gamestate.get_winner() != None:
-
-        print("\n\n")
-        print("IN if")
-        print(depth)
-        print("\n\n")
-
         return (minimax_eval(gamestate, cpu_color), None)
-    elif gamestate.get_turn() == cpu_color:
 
-        print("\n\n")
-        print("IN elif")
-        print("\n\n")
-
+    if gamestate.get_turn() == cpu_color:
+        print("MAXIMIZING PLAYER'S TURN")
         best_val = float("-inf")
         valid_moves = gamestate._valid_moves_exist(cpu_color, check_for_cpu=True)
-        print("\n\n")
-        print(valid_moves)
-        print("\n\n")
 
         for move in valid_moves:
             #move is a 4-tuple: (start_row, start_col, target_row, target_col)
             dummy_game = checkers.Checkers(init_config = gamestate.get_board(),
                                            init_turn = cpu_color)
-
-            print("\nMove made:")
-            print(move[0], move[1], move[2], move[3])
 
             dummy_game.make_move(move[0]+1, move[1]+1, move[2]+1, move[3]+1)
             (val, move_made) = minimax(dummy_game, cpu_color, depth-1)
@@ -94,23 +72,13 @@ def minimax(gamestate, cpu_color, depth):
         return (best_val, best_move)
 
     else: #If it's the minimizing player's turn to move...
-
-        print("\n\n")
-        print("IN else")
-        print("\n\n")
-
+        print("MINIMIZING PLAYER'S TURN")
         best_val = float("inf")
         valid_moves = gamestate._valid_moves_exist(player_color, check_for_cpu=True)
-        print("\n\n")
-        print(valid_moves)
-        print("\n\n")
 
         for move in valid_moves:
             dummy_game = checkers.Checkers(init_config = gamestate.get_board(),
                                            init_turn = player_color)
-
-            print("\nMove made:")
-            print(move[0], move[1], move[2], move[3])
 
             dummy_game.make_move(move[0]+1, move[1]+1, move[2]+1, move[3]+1)
             (val, move_made) = minimax(dummy_game, cpu_color, depth-1)
